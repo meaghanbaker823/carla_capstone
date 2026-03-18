07fbdc11851ecc95c4fa623a29f15f186f94c603
Meaghan Baker <meaghanbaker823@aol.com>
2026-03-18 12:38:17 -0400

    fixed lights testing

--- tests/collision_rule_test.py
+++ tests/collision_rule_test.py
@@ -1,1 +1,18 @@
 from unittest.mock import Mock
+import unittest
+
+
+# rule_flag testing
+class CollisionRuleTest(unittest.TestCase):
+    def test_rule_flag(self):
+        sensors1 = Mock()
+        sensors2 = Mock()
+
+        # sensors1 is mimicing no collision detected
+        sensors1.get_collisions().return_value = []
+        # sensor2 is mimicing a collision
+        sensors2.get_collisions().return_value = ["collision"]
+
+        
+
+# rule_follow testing
--- tests/lights_test.py
+++ tests/lights_test.py
@@ -2,45 +2,59 @@ import unittest
 from unittest.mock import Mock
 
 class LightTest(unittest.TestCase):
-    def test_light(self):
-        car = Mock()
-        lights = Mock()
 
-        lights.length = 3
-        lights.get_lights = [Mock(), Mock(), Mock()]
-
-        # setting all of the api call values for the mock object
-        lights.get_lights[0].is_light_close.return_value = True
-        lights.get_lights[0].get_color.return_value = "Red"
-        lights.get_lights[0].get_response.return_value = ""
-
-        lights.get_lights[1].is_light_close.return_value = True
-        lights.get_lights[1].get_color.return_value = "Green"
-        lights.get_lights[1].get_response.return_value = ""
-
-        lights.get_lights[2].is_light_close.return_value = False
-        lights.get_lights[2].get_color.return_value = "Yellow"
-        lights.get_lights[2].get_response.return_value = ""
-        
-
-        new_color = 'Red'
-        new_response = "stop"
-
-        for light in lights.get_lights:
+    def lights_to_test(self):
+        for light in self.lights.get_lights:
             if light.is_light_close():
                 old_color = light.get_color()
-                light.get_color.return_value = new_color
+                light.get_color.return_value = self.new_color
 
                 if old_color != light.get_color():
-                    light.get_response.return_value = new_response
+                    light.get_response.return_value = self.new_response
                     break
 
             else:
-                light.get_response.return_value = ""    
+                light.get_response.return_value = "" 
 
-        self.assertEqual(lights.get_lights[0].get_response(), "")  
-        self.assertEqual(lights.get_lights[1].get_response(), 'stop') 
-        self.assertEqual(lights.get_lights[2].get_response(), "")        
+    def setUp(self):
+        self.car = Mock()
+        self.lights = Mock()
+        self.lights.length = 3
+        self.lights.get_lights = [Mock()]
+
+        self.new_color = 'Red'
+        self.new_response = "stop"
+
+
+    def test_light0(self):
+        # setting all of the api call values for the mock object
+        self.lights.get_lights[0].is_light_close.return_value = True
+        self.lights.get_lights[0].get_color.return_value = "Red"
+        self.lights.get_lights[0].get_response.return_value = ""
+
+        self.lights_to_test() 
+
+        self.assertEqual(self.lights.get_lights[0].get_response(), "")        
+
+    def test_light1(self):
+        self.lights.get_lights[0].is_light_close.return_value = True
+        self.lights.get_lights[0].get_color.return_value = "Green"
+        self.lights.get_lights[0].get_response.return_value = ""
+
+        self.lights_to_test()
+
+        self.assertEqual(self.lights.get_lights[0].get_response(), 'stop')
+
+    def test_light2(self):
+        self.lights.get_lights[0].is_light_close.return_value = False
+        self.lights.get_lights[0].get_color.return_value = "Yellow"
+        self.lights.get_lights[0].get_response.return_value = ""
+
+        self.lights_to_test()
+        self.assertEqual(self.lights.get_lights[0].get_response(), "")
+    
+
+
                         
 
 
