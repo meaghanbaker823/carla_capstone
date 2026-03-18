c0cf913bb6d0bf7874e4ccfbaa3fc169e2ef3147
Meaghan Baker <meaghanbaker823@aol.com>
2026-03-18 14:37:26 -0400

    added collision rule testing

--- tests/collision_rule_test.py
+++ tests/collision_rule_test.py
@@ -1,18 +1,45 @@
 from unittest.mock import Mock
 import unittest
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from classes.collision_exception import CollisionErr
 
 
 # rule_flag testing
 class CollisionRuleTest(unittest.TestCase):
-    def test_rule_flag(self):
-        sensors1 = Mock()
-        sensors2 = Mock()
-
+    def setUp(self):
+        self.sensors = [Mock(), Mock()]
+    
+    def rule_flag(self):
+        if(self.sensors[1].get_collisions() != []):
+            raise CollisionErr("")
+        return True
+    
+    def test_rule_flag1(self):
         # sensors1 is mimicing no collision detected
-        sensors1.get_collisions().return_value = []
-        # sensor2 is mimicing a collision
-        sensors2.get_collisions().return_value = ["collision"]
+        self.sensors[1].get_collisions.return_value = []
 
-        
+        self.assertEqual(self.rule_flag(), True)    
+    
+    def test_rule_flag2(self):
+        # sensor2 is mimicing a collision
+        self.sensors[1].get_collisions.return_value = ["collision"]
+
+        with self.assertRaises(CollisionErr):
+            self.rule_flag()
+
 
 # rule_follow testing
+class CollisionRuleFollowTest(unittest.TestCase):
+    def rule_follow(self):
+        return 1, 0, None
+    def test_rule_follow(self):
+        self.assertEqual(self.rule_follow(),(1, 0, None) )
+
+
+
+
+if __name__ == "__main__":
+    unittest.main()
