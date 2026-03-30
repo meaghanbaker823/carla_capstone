a2ae6408151be71d3f13f00f14ba5652d7bd9838
Meaghan Baker <meaghanbaker823@aol.com>
2026-03-30 12:46:43 -0400

    parked rule

--- tests/parked_rule_test.py
+++ tests/parked_rule_test.py
@@ -1,16 +1,17 @@
 import unittest
 from unittest.mock import Mock
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from classes.parkedRule import ParkedRule
 # rule_flag testing
 class RuleFlag(unittest.TestCase):
-    def rule_flag(self, traffic_lights):
-        if(self.sensors[0].get_other_actors() != []):
-            if(self.sensors[0].get_detections()[-1][0].type_id[:-2].startswith('vehicle')):
-                return False
-        return True
-    
     def setUp(self):
-        self.lights = Mock()
-        self.sensors = [Mock(),]
+        self.sensors = [Mock(), Mock()]
+        self.car = Mock()
+        self.rule = ParkedRule(self.sensors, self.car)
+        self.limit = 30
         self.lights = Mock()
         self.detections = [[Mock(),],]
         self.sensors[0].get_detections.return_value = self.detections
@@ -19,27 +20,31 @@ class RuleFlag(unittest.TestCase):
     # other actors not empty, starts with vehicle -> returns False
     def test_case_one(self):
         self.detections[-1][0].type_id = "vehicle.ford.mustang"
-        self.assertEqual(self.rule_flag(self.lights), False)
+        self.assertEqual(self.rule.rule_flag(self.lights), False)
 
     # other actors not empty, not a vehicle -> returns True
     def test_case_two(self):
         self.detections[-1][0].type_id = "walker.pedestrian.0001"
         self.sensors[0].get_other_actors.return_value = [Mock(),]
-        self.assertEqual(self.rule_flag(self.lights), True)
+        self.assertEqual(self.rule.rule_flag(self.lights), True)
 
     # other actors empty, returns True
     def test_case_three(self):
         self.sensors[0].get_other_actors.return_value = []
-        self.assertEqual(self.rule_flag(self.lights), True)
+        self.assertEqual(self.rule.rule_flag(self.lights), True)
         
 
 # rule_follow testing
 class RuleFollow(unittest.TestCase):
-    def rule_follow(self):
-        return None, None, None
-        
+    def setUp(self):
+        self.sensors = [Mock(), Mock()]
+        self.car = Mock()
+        self.rule = ParkedRule(self.sensors, self.car)
+        self.lights = Mock()
+        self.limit = 30
+
     def test_rule_follow1(self):
-        self.assertEqual(self.rule_follow(), (None, None, None))
+        self.assertEqual(self.rule.rule_follow(self.lights, self.limit), (None, None, None))
 
     
 
