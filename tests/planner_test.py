ff4daa3cf0d41928f2c102dfe25de3bf6704fd72
Meaghan Baker <meaghanbaker823@aol.com>
2026-04-07 23:44:11 -0400

    planner testing done

--- tests/planner_test.py
+++ tests/planner_test.py
@@ -1,121 +1,67 @@
 from unittest.mock import Mock
+from unittest.mock import patch
 import unittest
+import sys
+import os
 
-# test maintain_speed
-class MaintainSpeedTesting(unittest.TestCase):
-    def setUp(self):
-        #checks = [SpeedOverCheck(speed_threshold), SpeedPerfectCheck(speed_threshold), SpeedUnderCheck(speed_threshold)]
-        self.checks = [Mock(), Mock(), Mock()]
-        self.limit = 30
-        self.new = 0
-        self.threshold = 3
-
-    
-    def maintain_speed(self):
-        new = 0
-        for check in self.checks:
-            if(check.speed_check(self.current, self.limit) != -1):
-                new = check.speed_check(self.current, self.limit)
-                break
-        
-        return new
-    
-    def test_maintain_speed1(self):
-        # none of the checks return -1
-        self.current = 28
-
-        # setting up checks
-        if(self.current >= self.limit):
-            self.checks[0].speed_check.return_value =  0
-        else:
-            self.checks[0].speed_check.return_value =  -1
-
-        if(self.current == (self.limit - self.threshold)):
-            self.checks[1].speed_check.return_value  = 0.3
-        else:
-            self.checks[1].speed_check.return_value  = -1
-
-        if(self.current < (self.limit - self.threshold)):
-            self.checks[2].speed_check.return_value =  0.8
-        else:
-            self.checks[2].speed_check.return_value =  -1
-
-        self.assertEqual(self.maintain_speed(), 0)
-
-    def test_maintain_speed2(self):
-        # need to change speed brach
-        self.current = 26
-
-        # setting up checks
-        if(self.current >= self.limit):
-            self.checks[0].speed_check.return_value =  0
-        else:
-            self.checks[0].speed_check.return_value =  -1
-
-        if(self.current == (self.limit - self.threshold)):
-            self.checks[1].speed_check.return_value  = 0.3
-        else:
-            self.checks[1].speed_check.return_value  = -1
-
-        if(self.current < (self.limit - self.threshold)):
-            self.checks[2].speed_check.return_value =  0.8
-        else:
-            self.checks[2].speed_check.return_value =  -1
-
-        self.assertEqual(self.maintain_speed(), 0.8)
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from classes.planner import Planner
     
 # test plan
 class PlannerTest(unittest.TestCase):
     def setUp(self):
-        self.speed = 30
-        self.new_plan = {"brake": None, "steering": None, "throttle": None}
-        self.observations = {"rules": [Mock(),], "steering_angle": 1, "distance": 20, "traffic_lights": Mock()}
-        self.checks = [Mock(), Mock(), Mock()]
-        self.limit = 30
-
-        # to interact with self. methods
-        self.planner = Mock()
+        self.planner = Planner()
+        self.observations = {"r": 0, "rules": [Mock(),], "traffic_lights": [Mock(), Mock()], "distance": 16, "route": [Mock(), Mock()],
+                             "new_waypoint": 0, "steering_angle": 1, "current_speed": 32.4}
+        self.DT = 0.005
+        self.extra = 6
+        self.u_nom = 1
+        self.alpha = 1.5
+        self.max_acc = 3
+        self.max_brake = 6
+        self.standard_dist = 8
         
-    
-    def plan(self):
-        # rules, navigation, maintain speed
-        for rule in self.observations["rules"]:
-            self.new_plan["brake"], self.limit, self.new_plan["steering"] = rule.rule_follow(self.observations["traffic_lights"], self.limit)
-        
-        if(self.new_plan["steering"] == None):
-            self.new_plan["steering"] = self.observations["steering_angle"]
+    # tests steering == none branch and new_speed == None and new_speed > 0 branch
+    @patch("classes.planner.CBFRule")
+    def test_no_steer_and_throttle_zero(self, mock_cbf):
+        self.observations["rules"][0].rule_follow.return_value = (None, None, None)
 
-        self.new_plan["throttle"] = self.planner.maintain_speed(self.speed, self.limit, self.checks)
-        if (self.new_plan["throttle"] == 0):
-            self.new_plan["brake"] = 1.0
-        else:
-            self.new_plan["brake"] = 0.0
-        
-        return self.new_plan
-    
-    def test_no_steer_and_throttle_zero(self):
-        self.observations["rules"][0].rule_follow.return_value = (1, 0, None)
-        self.planner.maintain_speed.return_value = 0
+        cbf = mock_cbf.return_value
+        cbf.calculate_min_distance.return_value = 8
+        cbf.calculate_safety_function.return_value = 8
+        cbf.calculate_allowable_distance.return_value = 0.6
+        cbf.final_logic.return_value = 0.6
 
-        self.assertDictEqual(self.plan(), {"brake": 1, "steering": 1, "throttle": 0})
+        with patch("builtins.print") as mock_print:
+            self.assertDictEqual(self.planner.plan(self.observations, self.DT, self.extra, self.u_nom, self.alpha, self.max_acc, self.max_brake, self.standard_dist), {"brake": 0, "steering": 1, "throttle": 0.6})
 
+    # tests steering != none branch and new_speed != None and new_speed <= 0 branch
     def test_steer_set_and_throttle_set(self):
-        self.observations["rules"][0].rule_follow.return_value = (0, self.limit, -1)
-        self.planner.maintain_speed.return_value = 0.8
-
-        self.assertDictEqual(self.plan(), {"brake": 0, "steering": -1, "throttle": 0.8})
+        self.observations["rules"][0].rule_follow.return_value = (1, 0, -1)
+        with patch("builtins.print") as mock_print:
+            self.assertDictEqual(self.planner.plan(self.observations, self.DT, self.extra, self.u_nom, self.alpha, self.max_acc, self.max_brake, self.standard_dist), {"brake": 1, "steering": -1, "throttle": 0})
    
 
 # test notify
 class NotifyTest(unittest.TestCase):
-    def notify(self):
-        return "The plan in this iteration is " + str(self.plan.get_new_plan())
-    
-    def test_notify(self):
-        self.plan = Mock()
-        self.plan.get_new_plan.return_value = {"brake": 1, "steering": 0, "throttle": None}
+    def setUp(self):
+        self.planner = Planner()
+        self.observations = {"r": 0, "rules": [Mock(),], "traffic_lights": [Mock(), Mock()], "distance": 16, "route": [Mock(), Mock()],
+                             "new_waypoint": 0, "steering_angle": 1, "current_speed": 32.4}
+        self.DT = 0.005
+        self.extra = 6
+        self.u_nom = 1
+        self.alpha = 1.5
+        self.max_acc = 3
+        self.max_brake = 6
+        self.standard_dist = 8
 
-        self.assertEqual(self.notify(), "The plan in this iteration is " + str({"brake": 1, "steering": 0, "throttle": None}))
+    def test_notify(self):
+        self.observations["rules"][0].rule_follow.return_value = (1, 0, -1)
+        with patch("builtins.print") as mock_print:
+            self.planner.plan(self.observations, self.DT, self.extra, self.u_nom, self.alpha, self.max_acc, self.max_brake, self.standard_dist)
+                                 
+        self.assertEqual(self.planner.notify(), "The plan in this iteration is " + str({"brake": 1, "steering": -1, "throttle": 0}))
 
 
 if __name__ == "__main__":
