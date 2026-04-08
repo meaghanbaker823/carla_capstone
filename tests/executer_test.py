ffe6feeac7cb4bc66c4068cd470ec810a9f5d202
Meaghan Baker <meaghanbaker823@aol.com>
2026-04-07 21:43:42 -0400

    executor testing

--- tests/executer_test.py
+++ tests/executer_test.py
@@ -1,45 +1,48 @@
 from unittest.mock import Mock
+from unittest.mock import patch
 import unittest
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from classes.executor import Executor
 
 # format_new_action test
+@patch("classes.executor.carla")
 class FormatNewActionTest(unittest.TestCase):
-    def format_new_action(self):
-        return ("The brake is: " + str(self.executer.get_new_action()["brake"]) +
-        ", the throttle is: " + str(self.executer.get_new_action()["throttle"]) + 
-        ", the steering is: " + str(self.executer.get_new_action()["steering"]))
-
-    def test_1(self):
-        # to interact with self. methods 
-        self.executer = Mock()
-        self.executer.get_new_action.return_value =  {"brake": 1, "steering": 0, "throttle": None}
-        self.assertEqual(self.format_new_action(), "The brake is: 1, the throttle is: None, the steering is: 0" )
+    def test_1(self, mock_carla): 
+        self.executer = Executor()
+        self.plan = {"brake": 1, "steering": 0, "throttle": None}
+        self.car = Mock()
+        mock_carla.VehicleControl.return_value = Mock()
+        with patch("builtins.print") as mock_print:
+            self.executer.execute(self.plan, self.car)
+        self.assertEqual(self.executer.format_new_action(), "The brake is: 1, the throttle is: None, the steering is: 0" )
 
 # execute test
-class ExecuteTest(unittest.TestCase):
-    def execute(self):
-        self.new_action = {"brake": self.plan["brake"], "steering": self.plan["steering"], "throttle": self.plan["throttle"]}
-                    
-        self.car.apply_control(self.carla.VehicleControl(throttle = self.plan["throttle"] ,steer = self.plan["steering"], brake = self.plan["brake"]))
-
-        return self.new_action
-    
-    def test_execute(self):
-        self.car = Mock()
-        self.carla = Mock()
+class ExecuteTest(unittest.TestCase): 
+    @patch("classes.executor.carla")
+    def test_execute(self, mock_carla):
+        self.executer = Executor()
         self.plan = {"brake": 1, "steering": 0, "throttle": None}
+        self.car = Mock()
+        mock_carla.VehicleControl.return_value = Mock()
 
-        self.assertEqual(self.execute(), self.plan)
+        with patch("builtins.print") as mock_print:
+            self.assertEqual(self.executer.execute(self.plan, self.car), self.plan)
 # notify test
 
-class NotifyTest(unittest.TestCase):
-    def notify(self):
-        return self.executer.format_new_action()
-    
-    def test_1(self):
-        self.executer = Mock()
-        self.executer.format_new_action.return_value = "The brake is: 1, the throttle is: None, the steering is: 0"
+class NotifyTest(unittest.TestCase):   
+    @patch("classes.executor.carla")
+    def test_1(self, mock_carla):
+        self.executer = Executor()
+        self.plan = {"brake": 1, "steering": 0, "throttle": None}
+        self.car = Mock()
+        mock_carla.VehicleControl.return_value = Mock()
 
-        self.assertEqual(self.notify(), "The brake is: 1, the throttle is: None, the steering is: 0")
+        with patch("builtins.print") as mock_print:
+            self.executer.execute(self.plan, self.car)
+            self.assertEqual(self.executer.notify(), "The brake is: 1, the throttle is: None, the steering is: 0")
 
 if __name__ == "__main__":
     unittest.main()
